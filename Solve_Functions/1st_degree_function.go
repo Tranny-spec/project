2ce06394ab78424ad12409db94775bc1d8e80111
tranny_spec <tranny0704@gmail.com>
2025-04-15 15:15:25 +0800

    Solve Function 5

--- Solve_Functions/1st_degree_function.go
+++ Solve_Functions/1st_degree_function.go
@@ -6,7 +6,7 @@ import (
 
 func SolveFirstEquation(a, b float32) float32 {
 
-	answer := float32(math.Floor(float64(b/a)*1000) / 1000)
+	answer := float32(math.Floor(float64(-b/a)*1000) / 1000)
 
 	return answer
 }
