47387f1b1fbe69426d5860fbd8038c166a07064b
Tranny-spec <tranny0704@gmail.com>
2025-05-16 12:49:33 +0800

    Final Version

--- Demo/Demo_Env.go
+++ Demo/Demo_Env.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//Import functions to solve equations from Solve_Functions
+	GUI "github.com/Tranny-spec/project/GUI"
+)
 
-func main() {
-	fmt.Println("Itegrated Development Enviroment for Assignment")
+func PrintResult(solutions int, answerslist []float32) {
+	fmt.Println("Number of solutions: ", solutions)
+
+	for i := 0; i < solutions; i++ {
+		fmt.Println("Answer ", i+1, ": ", answerslist[i])
+	}
 }
+func main() {
+	// fmt.Println("Itegrated Development Enviroment for Assignment")
+	// var solutions int
+	// var answerslist []float32
+
+	// solutions, answerslist = solveFunctions.SolveQuadraricEquation(2, -4, 5)
+	// PrintResult(solutions, answerslist)
+
+	GUI.DrawCubicGraph(2, -3, 5,-2, "Cubic Graph.png")
+
+}
+
